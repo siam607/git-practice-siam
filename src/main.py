5355e4a20c771067f272175eb8734559b5106509
siam607 <siamkhan545556@gmail.com>
2026-06-13 21:36:09 +0600

    Add multiply function to utils

--- src/main.py
+++ src/main.py
@@ -1,23 +1,14 @@
 import datetime
+from utils import add, subtract, multiply
 
 print("Name: Md. Sabbir Ahmed Siam")
 print("Today's Date:", datetime.date.today())
 
-
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-from utils import add, subtract
-
-
-print("Name: Md. Sabbir Ahmed Siam")
-print("Today's Date:", datetime.date.today())
-
-
+# Calculator functions call
 result_add = add(10, 5)
 result_sub = subtract(10, 5)
+result_mul = multiply(10, 5)
+
 print(f"Addition: {result_add}")
 print(f"Subtraction: {result_sub}")
+print(f"Multiplication: {result_mul}")
